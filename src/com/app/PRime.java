b0317ad951817d0195bea8b82d217c06f6492519
YOGESH29 PC <YOGESH29 PC@Yogesh>
2019-05-15 19:01:48 +0530

    change

--- src/com/app/PRime.java
+++ src/com/app/PRime.java
@@ -3,6 +3,7 @@ package com.app;
 public class PRime {
 
 	public static void main(String[] args) {
+		//ljaljfjlajaj
 		for(int i=1;i<=100;i++) {
 			boolean isPrime=true;
 			for(int j=2;j<i;j++) {
